c046eed4048479302ff3e09276a589b1df9d02c3
tobia <tobia.dipisa@geo-solutions.it>
2013-05-14 12:38:53 +0200

    Mode fixes for the login in chrome and remove operatesOn element as default from the service RNDT template

--- web/src/main/webapp/scripts/geonetwork.js
+++ web/src/main/webapp/scripts/geonetwork.js
@@ -70,7 +70,7 @@ function get_cookie ( cookie_name )
 
 // Forms
 	function goSubmit(form_name) {
-		document.forms[form_name].onsubmit();
+		document.forms[form_name].submit();
 	}
 
 	function goReset(form_name)
